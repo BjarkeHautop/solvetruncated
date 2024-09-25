4cb85f67b5b1fafca2be8c1453038f7265626366
Bjarke Hautop <bjarke.hautop@gmail.com>
2024-09-25 22:09:21 +0200

    Refactor solve_truncated_normal function to reduce complexity

--- R/solve_truncated_normal.R
+++ R/solve_truncated_normal.R
@@ -17,10 +17,25 @@
 #'
 #' ## Print output of optim by setting verbose=TRUE
 #' solve_truncated_normal(2, 1, 0.3, verbose = TRUE)
-solve_truncated_normal <- function(desired_mean, x_value, desired_prob, a = 0,
-                                   b = Inf, initial_guess = c(desired_mean, 1),
+solve_truncated_normal <- function(desired_mean,
+                                   x_value,
+                                   desired_prob,
+                                   a = 0,
+                                   b = Inf,
+                                   initial_guess = c(desired_mean, 1),
                                    verbose = FALSE) {
-  # Check for logical consistency and edge cases
+  validate_inputs(desired_mean, x_value, desired_prob, a, b)
+
+  objective_function <- function(params) {
+    calculate_error(params, x_value, desired_mean, desired_prob, a, b)
+  }
+
+  result <- optim(initial_guess, objective_function)
+  handle_optimization_result(result, verbose)
+}
+
+# Validates the inputs to the main function
+validate_inputs <- function(desired_mean, x_value, desired_prob, a, b) {
   if (a >= b) {
     stop("Error: Lower bound 'a' must be less than upper bound 'b'.")
   }
@@ -33,47 +48,34 @@ solve_truncated_normal <- function(desired_mean, x_value, desired_prob, a = 0,
   if (x_value <= a || x_value >= b) {
     stop("Error: x_value must be within the bounds [a, b].")
   }
+}
 
-  # Define the objective function to minimize
-  objective_function <- function(params) {
-    mu <- params[1]
-    sigma <- params[2]
+# Calculates the error for given parameters
+# Follows syntax on https://en.wikipedia.org/wiki/Truncated_normal_distribution
+calculate_error <- function(params, x_value, desired_mean, desired_prob, a, b) {
+  mu <- params[1]
+  sigma <- params[2]
 
-    # Penalize if parameters go out of bounds
-    if (mu <= a || mu >= b || sigma <= 0) {
-      return(Inf)
-    }
+  if (mu <= a || mu >= b || sigma <= 0) return(Inf)
 
-    # Following syntax given on https://en.wikipedia.org/wiki/Truncated_normal_distribution
-    xi <- (x_value - mu) / sigma
-    alpha <- (a - mu) / sigma
-    beta <- (b - mu) / sigma
-    Z <- pnorm(beta) - pnorm(alpha)
+  xi <- (x_value - mu) / sigma
+  alpha <- (a - mu) / sigma
+  beta <- (b - mu) / sigma
+  z <- pnorm(beta) - pnorm(alpha)
 
-    mean_trunc <- mu + (dnorm(alpha) - dnorm(beta)) / Z * sigma
-    prob_trunc <- (pnorm(xi) - pnorm(alpha)) / Z
+  mean_trunc <- mu + (dnorm(alpha) - dnorm(beta)) / z * sigma
+  prob_trunc <- (pnorm(xi) - pnorm(alpha)) / z
 
-    # Calculate the sum of squared differences
-    error <- abs(mean_trunc - desired_mean) + abs(prob_trunc - desired_prob)
+  abs(mean_trunc - desired_mean) + abs(prob_trunc - desired_prob)
+}
 
-    return(error)
-  }
-
-  # Optimize the parameters
-  result <- optim(initial_guess, objective_function)
-
-  # Check for convergence or failure
+# Handles the optimization result and prints if needed
+handle_optimization_result <- function(result, verbose) {
   if (result$convergence != 0) {
     warning("Optimization did not converge.")
   }
-
   if (verbose) {
     print(result)
   }
-
-  # Extract the optimal parameters
-  optimal_mu <- result$par[1]
-  optimal_sigma <- result$par[2]
-
-  return(list(mu = optimal_mu, sigma = optimal_sigma))
+  list(mu = result$par[1], sigma = result$par[2])
 }
